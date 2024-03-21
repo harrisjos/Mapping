824c89b503c6fd220d7613cbdd19a92f85cf6b4b
harrisjos <harro.jnr@gmail.com>
2024-03-22 10:19:56 +1100

    Init Comit

---
+++ Files/leadDaysData.js
@@ -0,0 +1,332 @@
+var leadDayData = {
+    "routes": [{
+      "RouteCode": "MELMEL",
+      "FROM": "MEL",
+      "TO": "MEL",
+      "Days": 1,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "MELNSW",
+      "FROM": "MEL",
+      "TO": "NSW",
+      "Days": 2,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "MELQLD",
+      "FROM": "MEL",
+      "TO": "QLD",
+      "Days": 3,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "MELSA",
+      "FROM": "MEL",
+      "TO": "SA",
+      "Days": 2,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "MELWA",
+      "FROM": "MEL",
+      "TO": "WA",
+      "Days": 5,
+      "Leaves on": "Saturday",
+      "Leaves On Number": 6
+    }, {
+      "RouteCode": "MELACT",
+      "FROM": "MEL",
+      "TO": "ACT",
+      "Days": 3,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "MELTAS",
+      "FROM": "MEL",
+      "TO": "TAS",
+      "Days": 5,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "MELNT",
+      "FROM": "MEL",
+      "TO": "NT",
+      "Days": 7,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "WAWA",
+      "FROM": "WA",
+      "TO": "WA",
+      "Days": 1,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "WAMEL",
+      "FROM": "WA",
+      "TO": "MEL",
+      "Days": 4,
+      "Leaves on": "Tuesday",
+      "Leaves On Number": 2
+    }, {
+      "RouteCode": "WANSW",
+      "FROM": "WA",
+      "TO": "NSW",
+      "Days": 6,
+      "Leaves on": "Tuesday",
+      "Leaves On Number": 2
+    }, {
+      "RouteCode": "WAQLD",
+      "FROM": "WA",
+      "TO": "QLD",
+      "Days": 7,
+      "Leaves on": "Tuesday",
+      "Leaves On Number": 2
+    }, {
+      "RouteCode": "WASA",
+      "FROM": "WA",
+      "TO": "SA",
+      "Days": 5,
+      "Leaves on": "Tuesday",
+      "Leaves On Number": 2
+    }, {
+      "RouteCode": "WAACT",
+      "FROM": "WA",
+      "TO": "ACT",
+      "Days": 7,
+      "Leaves on": "Tuesday",
+      "Leaves On Number": 2
+    }, {
+      "RouteCode": "WATAS",
+      "FROM": "WA",
+      "TO": "TAS",
+      "Days": 9,
+      "Leaves on": "Tuesday",
+      "Leaves On Number": 2
+    }, {
+      "RouteCode": "WANT",
+      "FROM": "WA",
+      "TO": "NT",
+      "Days": 11,
+      "Leaves on": "Tuesday",
+      "Leaves On Number": 2
+    }, {
+      "RouteCode": "QLDQLD",
+      "FROM": "QLD",
+      "TO": "QLD",
+      "Days": 1,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "QLDMEL",
+      "FROM": "QLD",
+      "TO": "MEL",
+      "Days": 3,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "QLDNSW",
+      "FROM": "QLD",
+      "TO": "NSW",
+      "Days": 2,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "QLDSA",
+      "FROM": "QLD",
+      "TO": "SA",
+      "Days": 4,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "QLDWA",
+      "FROM": "QLD",
+      "TO": "WA",
+      "Days": 7,
+      "Leaves on": "Thursday",
+      "Leaves On Number": 4
+    }, {
+      "RouteCode": "QLDACT",
+      "FROM": "QLD",
+      "TO": "ACT",
+      "Days": 4,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "QLDTAS",
+      "FROM": "QLD",
+      "TO": "TAS",
+      "Days": 8,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "QLD NT",
+      "FROM": "QLD",
+      "TO": "NT",
+      "Days": 10,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "SASA",
+      "FROM": "SA",
+      "TO": "SA",
+      "Days": 1,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "SAMEL",
+      "FROM": "SA",
+      "TO": "MEL",
+      "Days": 2,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "SANSW",
+      "FROM": "SA",
+      "TO": "NSW",
+      "Days": 3,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "SAQLD",
+      "FROM": "SA",
+      "TO": "QLD",
+      "Days": 4,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "SAWA",
+      "FROM": "SA",
+      "TO": "WA",
+      "Days": 1,
+      "Leaves on": "Sunday",
+      "Leaves On Number": 7
+    }, {
+      "RouteCode": "SAACT",
+      "FROM": "SA",
+      "TO": "ACT",
+      "Days": 4,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "SATAS",
+      "FROM": "SA",
+      "TO": "TAS",
+      "Days": 7,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "SANT",
+      "FROM": "SA",
+      "TO": "NT",
+      "Days": 5,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "NSWNSW",
+      "FROM": "NSW",
+      "TO": "NSW",
+      "Days": 1,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "NSWMEL",
+      "FROM": "NSW",
+      "TO": "MEL",
+      "Days": 2,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "NSWQLD",
+      "FROM": "NSW",
+      "TO": "QLD",
+      "Days": 2,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "NSWSA",
+      "FROM": "NSW",
+      "TO": "SA",
+      "Days": 3,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "NSWWA",
+      "FROM": "NSW",
+      "TO": "WA",
+      "Days": 7,
+      "Leaves on": "Thursday",
+      "Leaves On Number": 4
+    }, {
+      "RouteCode": "NSWACT",
+      "FROM": "NSW",
+      "TO": "ACT",
+      "Days": 2,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "NSWTAS",
+      "FROM": "NSW",
+      "TO": "TAS",
+      "Days": 7,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "NSWNT",
+      "FROM": "NSW",
+      "TO": "NT",
+      "Days": 9,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "ACTMEL",
+      "FROM": "ACT",
+      "TO": "MEL",
+      "Days": 3,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "ACTNSW",
+      "FROM": "ACT",
+      "TO": "NSW",
+      "Days": 2,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "ACTQLD",
+      "FROM": "ACT",
+      "TO": "QLD",
+      "Days": 4,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "ACTSA",
+      "FROM": "ACT",
+      "TO": "SA",
+      "Days": 4,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "ACTWA",
+      "FROM": "ACT",
+      "TO": "WA",
+      "Days": 7,
+      "Leaves on": "Thursday",
+      "Leaves On Number": 4
+    }, {
+      "RouteCode": "ACTTAS",
+      "FROM": "ACT",
+      "TO": "TAS",
+      "Days": 8,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }, {
+      "RouteCode": "ACTNT",
+      "FROM": "ACT",
+      "TO": "NT",
+      "Days": 10,
+      "Leaves on": "Any",
+      "Leaves On Number": 0
+    }]
+  }
